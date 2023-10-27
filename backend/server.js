2bab75a34871711489a656a17120bd4cf64ec57f
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2023-10-27 18:03:07 -0300

    mongodb config

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
-
+import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
+
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+//every time we hits this path we go to productRoutes
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
