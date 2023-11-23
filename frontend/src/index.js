2563d55bf099df8ca7077c6851da968c6ae6e78d
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2023-11-23 15:18:44 -0300

    redux config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import './assets/styles/bootstrap.custom.css';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import { Provider } from 'react-redux';
+import store from './store';
 
 
 const router = createBrowserRouter(
@@ -21,7 +23,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
